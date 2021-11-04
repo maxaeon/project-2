d7adf2f84544049561a7671999a410ccedfec604
Tevissaur <berserkboar@protonmail.com>
2021-11-04 13:37:40 -0500

    able to log in now

--- public/js/login.js
+++ public/js/login.js
@@ -1,98 +1,50 @@
-// // Selectors
-// // NavBar 
-// const navLoginLink = document.querySelector('#loginWindow');
 
-// const navRegisterLink = document.querySelector('#registerWindow');
-
-// // Cards
-// const loginCard = document.querySelector('#loginCard');
-// const registerCard = document.querySelector('#registerCard');
-// // Event Listener
-// navLoginLink.addEventListener('click', () => {
-
-//   if (loginCard.style.display === 'none') {
-//     loginCard.style.display = 'block';
-//   } else {
-//     registerCard.style.display = 'none'
-//   }
-// });
-
-// navRegisterLink.addEventListener('click', () => {
-//   if (registerCard.style.display === 'none') {
-//     registerCard.style.display = 'block';
-//   } else {
-//     loginCard.style.display = 'none'
-//   }
-// });
-
-// const loginFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   // Collect values from the login form
-//   const email = document.querySelector('#email-login').value.trim();
-//   const password = document.querySelector('#password-login').value.trim();
-
-//   if (email && password) {
-//     // Send a POST request to the API endpoint
-//     const response = await fetch('/api/users/login', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         email,
-//         password
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//     });
-
-//     if (response.ok) {
-//       // If successful, redirect the browser to the profile page
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
-
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const name = document.querySelector('#name-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
-
-//   if (name && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         name,
-//         email,
-//         password
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
-
-// document
-//   .querySelector('.login-form')
-//   .addEventListener('submit', loginFormHandler);
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
+// const email = document.getElementById('login-email')
+// const password = document.getElementById('login-password')
+// const signInBtn = document.getElementById('sign-in-button')
 
 
-// //  If login is selected display login card
-// // else hide registration card
 
-// // If Registration is selected display register card
-// // Else hide login card
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.getElementById('login-email').value.trim();
+  const password = document.getElementById('login-password').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+
+
+document
+  .getElementById('login-form')
+  .addEventListener('submit', loginFormHandler);
+
+
+
+
+//  If login is selected display login card
+// else hide registration card
+
+// If Registration is selected display register card
+// Else hide login card
