1412dfadaa7d0d79ed0e29fcf231e2adf2cf3254
TS93 <tschram93@yahoo.com>
2021-11-06 16:02:29 -0400

    weather section working

--- public/js/zone.js
+++ public/js/zone.js
@@ -12,7 +12,6 @@
 // });
 
 // Selectors
-const button = document.querySelector('.btn');
 const citySearch = document.querySelector('#cityWeatherSearch');
 const weatherBtn = document.querySelector('#weatherBtn');
 const weatherLink = document.querySelector('#weatherZone');
@@ -26,12 +25,10 @@ const weatherStatus = document.querySelector('#weatherStatus');
 // Function
 
 // Event Listener
-button.addEventListener('click', (e) => {
-    e.preventDefault();
+weatherBtn.addEventListener('click', () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+citySearch.value+`&appid=96f5a24f18a847ade76f1f997da772d5&units=imperial`)
-        .then(data => {
-            console.log(data);
-        data.json()})
+        .then(response => 
+        response.json())
         .then(data => {
                 let descValue = data['weather'][0]['description'];
                 let humidityValue = data['main']['humidity'];
@@ -40,5 +37,5 @@ button.addEventListener('click', (e) => {
             humidityInsert.innerText = humidityValue;
             weatherStatus.innerText = descValue;
         })
-        .catch(err => alert('Need to Enter a valid city'))
+        .catch(err => console.log('Need to Enter a valid city'))
 });
