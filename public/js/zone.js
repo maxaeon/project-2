9cae6f3ac646d92c8e9841812f08557e1bcd0961
TS93 <tschram93@yahoo.com>
2021-11-06 15:25:54 -0400

    commit as savepoint

--- public/js/zone.js
+++ public/js/zone.js
@@ -18,15 +18,27 @@ const weatherBtn = document.querySelector('#weatherBtn');
 const weatherLink = document.querySelector('#weatherZone');
 console.log(citySearch);
 
+// Weather Section Selectors
+const humidityInsert = document.querySelector('#humidityInsert');
+const tempInsert = document.querySelector('#tempInsert');
+const weatherStatus = document.querySelector('#weatherStatus');
+
 // Function
 
 // Event Listener
 button.addEventListener('click', (e) => {
     e.preventDefault();
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+citySearch.value+`&appid=96f5a24f18a847ade76f1f997da772d5&units=imperial`)
-        .then(response => {
-            console.log(response);
-        response.json()})
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+        data.json()})
+        .then(data => {
+                let descValue = data['weather'][0]['description'];
+                let humidityValue = data['main']['humidity'];
+                let tempValue = data['main']['temp'];
+            tempInsert.innerText = tempValue;
+            humidityInsert.innerText = humidityValue;
+            weatherStatus.innerText = descValue;
+        })
         .catch(err => alert('Need to Enter a valid city'))
 });
