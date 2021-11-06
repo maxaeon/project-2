ae459e4dafe52ed671fd7436238e072439724077
TS93 <tschram93@yahoo.com>
2021-11-06 16:07:31 -0400

    removed comments

--- public/js/zone.js
+++ public/js/zone.js
@@ -1,30 +1,11 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve("views/partials", "dashboard-weather.handlebars"));
-// });
-
-// app.listen(3001, () => {
-//     console.log(`listening at http://localhost:3001}`)
-// });
-
-// Selectors
 const citySearch = document.querySelector('#cityWeatherSearch');
 const weatherBtn = document.querySelector('#weatherBtn');
 const weatherLink = document.querySelector('#weatherZone');
-console.log(citySearch);
 
-// Weather Section Selectors
 const humidityInsert = document.querySelector('#humidityInsert');
 const tempInsert = document.querySelector('#tempInsert');
 const weatherStatus = document.querySelector('#weatherStatus');
 
-// Function
-
-// Event Listener
 weatherBtn.addEventListener('click', () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+citySearch.value+`&appid=96f5a24f18a847ade76f1f997da772d5&units=imperial`)
         .then(response => 
