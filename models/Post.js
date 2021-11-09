9639fcce2c53b10a2b258d956caae4d3448fff9e
Tevissaur <berserkboar@protonmail.com>
2021-11-09 10:13:27 -0600

    adding images and fixed profile bugs

--- models/Post.js
+++ models/Post.js
@@ -3,32 +3,32 @@ const sequelize = require("../config/connection");
 
 // create our Post model
 class Post extends Model {
-  // JS built in static keyword indicates upvote method is based on Post model
-  // and not an instance method like User model
-  // Post.upvote() is a custom static Sequelize model method
-  static upvote(body, models) {
-    //   pass the Vote model in as an argument from post-routes.js
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id,
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id,
-        },
-        include: [
-            {
-              model: models.Comment,
-              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-              include: {
-                model: models.User,
-                attributes: ['username']
-              }
-            }
-          ]
-      });
-    });
-  }
+  // // JS built in static keyword indicates upvote method is based on Post model
+  // // and not an instance method like User model
+  // // Post.upvote() is a custom static Sequelize model method
+  // static upvote(body, models) {
+  //   //   pass the Vote model in as an argument from post-routes.js
+  //   return models.Vote.create({
+  //     user_id: body.user_id,
+  //     post_id: body.post_id,
+  //   }).then(() => {
+  //     return Post.findOne({
+  //       where: {
+  //         id: body.post_id,
+  //       },
+  //       include: [
+  //           {
+  //             model: models.Comment,
+  //             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+  //             include: {
+  //               model: models.User,
+  //               attributes: ['username']
+  //             }
+  //           }
+  //         ]
+  //     });
+  //   });
+  // }
 }
 
 // create fields/columns for Post model
